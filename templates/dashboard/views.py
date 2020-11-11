109f3f05b9d3f3c5126825453a77a6eb3665fd64
Jacob Shin <jacobshin313@gmail.com>
2020-11-10 21:30:51 -0500

    Add gallon var to heatmap

--- templates/dashboard/views.py
+++ templates/dashboard/views.py
@@ -3,6 +3,6 @@ dashboard_blueprint = Blueprint('dashboard',__name__)
 
 @dashboard_blueprint.route('/')
 def index():
-	return render_template("index.html")
+    return render_template("index.html")
 
 # @dashboard_blueprint.route('/dashboard')
--- templates/static/js/HeatMap.js
+++ templates/static/js/HeatMap.js
@@ -9,54 +9,58 @@ import './style.css';
 const today = new Date();
 
 export default function HeatMap() {
-  const randomValues = getRange(350).map(index => {
-    return {
-      date: shiftDate(today, -index),
-      count: getRandomInt(1, 4),
-    };
-  });
-  return (
-    <div>
-      <Title>Water Usage This Year</Title>
-      {/* <p>Random values with onClick and react-tooltip</p> */}
+    const randomValues = getRange(350).map(index => {
+        let range = 100 - 1;
+        const rando = getRandomInt(1, 99);
+        var cnt = Math.floor(4 * rando / (range + 1)) + 1;
+        return {
+            date: shiftDate(today, -index),
+            gallons: rando,
+            count: cnt,
+        };
+    });
+    return (
+        <div>
+            <Title>Water Usage This Year</Title>
+            {/* <p>Random values with onClick and react-tooltip</p> */}
 
-      <CalendarHeatmap
-        startDate={shiftDate(today, -300)}
-        endDate={today}
-        values={randomValues}
-        classForValue={value => {
-          if (!value) {
-            return 'color-empty';
-          }
-          return `color-water-${value.count}`;
-        }}
-        tooltipDataAttrs={value => {
-          return {
-            'data-tip': `${value.date.toISOString().slice(0, 10)} has count: ${
-              value.count
-            }`,
-          };
-        }}
-        showWeekdayLabels={true}
-        onClick={value => alert(`Clicked on value with count: ${value.count}`)}
-      />
-      <ReactTooltip />
-    </div>
-  );
+            <CalendarHeatmap
+                startDate={shiftDate(today, -300)}
+                endDate={today}
+                values={randomValues}
+                classForValue={value => {
+                    if (!value) {
+                        return 'color-empty';
+                    }
+                    return `color-water-${value.count}`;
+                }}
+                tooltipDataAttrs={value => {
+                    return {
+                        'data-tip': `On ${value.date.toISOString().slice(0, 10)} you used: ${
+              value.gallons
+            } gallons.`,
+                    };
+                }}
+                showWeekdayLabels={true}
+                onClick={value => alert(`Clicked on a day with : ${value.gallons} gallons`)}
+            />
+            <ReactTooltip />
+        </div>
+    );
 }
 
 function shiftDate(date, numDays) {
-  const newDate = new Date(date);
-  newDate.setDate(newDate.getDate() + numDays);
-  return newDate;
+    const newDate = new Date(date);
+    newDate.setDate(newDate.getDate() + numDays);
+    return newDate;
 }
 
 function getRange(count) {
-  return Array.from({ length: count }, (_, i) => i);
+    return Array.from({ length: count }, (_, i) => i);
 }
 
 function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 // const rootElement = document.getElementById('root');
