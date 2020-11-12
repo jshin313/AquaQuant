543c939eedaa1c704f0693212f306a6c1277eb13
Jacob Shin <jacobshin313@gmail.com>
2020-11-11 21:58:55 -0500

    Fix not found error when manually type in url

--- templates/dashboard/views.py
+++ templates/dashboard/views.py
@@ -2,6 +2,8 @@ from flask import render_template, Blueprint
 dashboard_blueprint = Blueprint('dashboard',__name__)
 
 @dashboard_blueprint.route('/')
+@dashboard_blueprint.route('/sensors')
+@dashboard_blueprint.route('/aboutwater')
 def index():
     return render_template("index.html")
 
