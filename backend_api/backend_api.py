45384dc6d37e96ce6a600d488b4878a2dc7b6904
Jacob Shin <jacobshin313@gmail.com>
2020-11-17 21:39:10 -0500

    Implement most of get and post for Day api

--- backend_api/backend_api.py
+++ backend_api/backend_api.py
@@ -5,35 +5,61 @@ from exts import db
 from models import Stats
 import json
 
+from sqlalchemy.ext.declarative import DeclarativeMeta
+
+# Code for AlchemyEncoder from https://stackoverflow.com/questions/5022066/how-to-serialize-sqlalchemy-result-to-json
+class AlchemyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj.__class__, DeclarativeMeta):
+            # an SQLAlchemy class
+            fields = {}
+            for field in [x for x in dir(obj) if not x.startswith('_') and x != 'metadata' and "query" not in x]:
+                data = obj.__getattribute__(field)
+                try:
+                    json.dumps(data) # this will fail on non-encodable values, like other classes
+                    fields[field] = data
+                except TypeError:
+                    fields[field] = None
+            # a json-encodable dict
+            return fields
+
+        return json.JSONEncoder.default(self, obj)
+
 class Day(Resource):
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
-        self.reqparse.add_argument('date', type = str, required=True)
         self.reqparse.add_argument('data', type = str)
+        self.reqparse.add_argument('date', type = str)
         super(Day, self).__init__()
 
     # Get all the data for a day
     def get(self):
         args = self.reqparse.parse_args()
+        date = args['date']
+        stats = Stats.query.filter_by(date=date).all()
+        # print(stats)
+
+        data = json.dumps(stats, cls=AlchemyEncoder)
+
+
         return  {
-            'date': args['date'],
+            'data': data,
         }, 200
 
     # Create an entry in database for a specific time frame
     def post(self):
         args = self.reqparse.parse_args()
-        date = args['date']
         data = json.loads(args['data'])
 
-        stats = Stats(date=date, start_time=data['start_time'], end_time=data['end_time'], water_source=data['watersource'])
+        stats = Stats(date=data['date'], start_time=data['start_time'], end_time=data['end_time'], water_source=data['watersource'])
 
-        # print(stats)
-        if Stats.query.filter_by(date=date, start_time=data['start_time'], end_time=data['end_time'], water_source=data['watersource']) is None:
+        # Prevent duplicate entrires
+        if Stats.query.filter_by(date=data['date'], start_time=data['start_time'], end_time=data['end_time'], water_source=data['watersource']).first() is None:
+            # Add entry if not duplicate
             db.session.add(stats)
             db.session.commit()
 
         return  {
-            'date': date,
             'data': data,
         }, 200
 
--- test.py
+++ test.py
@@ -1,16 +1,21 @@
 import requests, json
 
-payload = {
+payload1 = {
     'date': '10-14-2020',
+}
+
+payload2 = {
     'data': json.dumps({
+        'date': '10-14-2020',
         'start_time': '17:00:20',
         'end_time': '17:00:59',
         'watersource': 'faucet',
     })
 }
 
-r = requests.post('http://localhost:5000/api/day', data=payload).json()
+r = requests.get('http://localhost:5000/api/day', data=payload1).json()
+# r = requests.post('http://localhost:5000/api/day', data=payload2).json()
 
 # print(r['date'])
 # print(json.loads(r['data'])['start_time'])
-print(r['data']['start_time'])
+print(r)
