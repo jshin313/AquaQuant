dc061f28b29a398f7486d455dbcb60900d996686
Jacob Shin <jacobshin313@gmail.com>
2020-11-22 14:27:54 -0500

    Make socketio not async

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ from exts import db
 from flask_socketio import SocketIO, emit
 
 from time import sleep
-from threading import Thread, Event
 
 def register_extensions(app):
     db.init_app(app)
@@ -48,70 +47,14 @@ api.add_resource(Day, '/api/day', endpoint='day')
 api.add_resource(Year, '/api/year', endpoint='year')
 api.add_resource(On, '/api/on', endpoint='on')
 
-socketio = SocketIO(app, async_mode=None, logger=True, engineio_logger=True)
-# socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app, cors_allowed_origins="*")
 
-thread1 = Thread()
-thread2 = Thread()
-thread3 = Thread()
-thread_stop_event = Event()
-
-def faucet():
-    """
-    Generate a random number every 1 second and emit to a socketio instance (broadcast)
-    Ideally to be run in a separate thread?
-    """
-    #infinite loop of magical random numbers
-    print("Making random numbers")
-    while not thread_stop_event.isSet():
-        socketio.emit('faucet', {'on': is_on['faucet']}, namespace='/test')
-        socketio.sleep(0.9)
-
-def shower():
-    """
-    Generate a random number every 1 second and emit to a socketio instance (broadcast)
-    Ideally to be run in a separate thread?
-    """
-    #infinite loop of magical random numbers
-    print("Making random numbers")
-    while not thread_stop_event.isSet():
-        socketio.emit('shower', {'on': is_on['shower']}, namespace='/test')
-        socketio.sleep(0.9)
-
-def toilet():
-    """
-    Generate a random number every 1 second and emit to a socketio instance (broadcast)
-    Ideally to be run in a separate thread?
-    """
-    #infinite loop of magical random numbers
-    print("Making random numbers")
-    while not thread_stop_event.isSet():
-        number = round(random()*10, 3)
-        print(number)
-        socketio.emit('toilet', {'on': is_on['toilet']}, namespace='/test')
-        socketio.sleep(0.9)
 
 @socketio.on('connect', namespace='/test')
-def test_connect():
-    # need visibility of the global thread object
-    global thread1
-    global thread2
-    global thread3
-    print('Client connected')
-
-    #Start the random number generator thread only if the thread has not been started before.
-    if not thread1.isAlive():
-        print("Starting Thread")
-        thread1 = socketio.start_background_task(faucet)
-
-    if not thread2.isAlive():
-        print("Starting Thread")
-        thread2 = socketio.start_background_task(shower)
-
-    if not thread3.isAlive():
-        print("Starting Thread")
-        thread3 = socketio.start_background_task(toilet)
-
+def faucet():
+    socketio.emit('faucet', {'on': is_on['faucet']}, namespace='/test')
+    socketio.emit('shower', {'on': is_on['shower']}, namespace='/test')
+    socketio.emit('toilet', {'on': is_on['toilet']}, namespace='/test')
 
 # # Handler for a message recieved over 'connect' channel
 # @socketio.on('faucet_status')
